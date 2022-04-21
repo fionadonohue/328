639de5ab9db6ac3d600ef8d7ddc9bdb4b58b4a3e
Fiona Donohue <fionadonohue@Fionas-MacBook.local>
2022-04-20 19:39:09 -0700

    up

--- webapp.py
+++ webapp.py
@@ -1,19 +1,22 @@
-from flask import Flask, url_for, render_template, request
+from flask import Flask, url_for, render_template, request, Markup
 import json
 
 webapp = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-@webapp.route("/", methods = ["POST", "GET"])
+@webapp.route("/", methods = ["GET"])
 def home():
-    if request.method == "POST": #if submit button is hit and data is updated
+    return render_template("home.html")
+    if request.method == "GET": #if submit button is hit and data is updated
         with open('./demographics.json') as json_file:
             data = json.load(json_file)
-        county = request.form["county"]
+        county = request.args["county"]
 
         ten_pop, fourteen_pop, percent_change, county_name, med_income, ownership_rate, households, white_not_hispanic, bi_racial, asian_alone, pacific_islander, white_alone, hispanic_alone, black_alone, native_alone = get_info(data, county)
 
 #population route...
-
+@webapp.route("/populations", methods = ["GET"])
+def populations():
+    if "county" in request.args:
         return render_template("populations.html", countyname = county_name , json = data, ten_population = ten_pop , fourteen_population = fourteen_pop, percent_population = percent_change, )
     else:
         with open('./demographics.json') as json_file:
@@ -23,13 +26,13 @@ def home():
 #...population route
 
 #housing route...
-@webapp.route("/housing", methods = ["POST", "GET"])
+@webapp.route("/housing", methods = ["GET"])
 def housing():
-    if request.method == "POST": #if submit button is hit and data is updated
+    if "county" in request.args:
         with open('./demographics.json') as json_file:
             data = json.load(json_file)
 
-        county = request.form["county"]
+        county = request.args["county"]
 
         ten_pop, fourteen_pop, percent_change, county_name, med_income, ownership_rate, households, white_not_hispanic, bi_racial, asian_alone, pacific_islander, white_alone, hispanic_alone, black_alone, native_alone = get_info(data, county)
 
@@ -37,22 +40,26 @@ def housing():
     else:
         with open('./demographics.json') as json_file:
             data = json.load(json_file)
+        options = ""
+        for d in data:
+            options += Markup('<option value="' + d["County"] + '">' + d["County"] + "</option>")
 
-        return render_template("housing.html", json = data)
+
+        return render_template("housing.html", housing = options)
 #...housing route
 
 #dversity route...
-@webapp.route("/diversity", methods = ["POST", "GET"])
+@webapp.route("/diversity", methods = ["GET"])
 def diversity():
-    if request.method == "POST": #if submit button is hit and data is updated
+    if "county" in request.args:
         with open('./demographics.json') as json_file:
             data = json.load(json_file)
 
-        county = request.form["county"]
+        county = request.args["county"]
 
         ten_pop, fourteen_pop, percent_change, county_name, med_income, ownership_rate, households, white_not_hispanic, bi_racial, asian_alone, pacific_islander, white_alone, hispanic_alone, black_alone, native_alone = get_info(data, county)
 
-        return render_template("housing.html", countyname = county_name , json = data, whitenothispanic = white_not_hispanic, biracial = bi_racial, asianalone = asian_alone, pacificislander = pacific_islander, white = white_alone, hispanic = hispanic_alone, black = black_alone, native = native_alone)
+        return render_template("diversity.html", countyname = county_name , json = data, whitenothispanic = white_not_hispanic, biracial = bi_racial, asianalone = asian_alone, pacificislander = pacific_islander, white = white_alone, hispanic = hispanic_alone, black = black_alone, native = native_alone)
     else:
         with open('./demographics.json') as json_file:
             data = json.load(json_file)
@@ -79,14 +86,14 @@ def get_info(data, county):
             #...housing
 
 #diversity...
-white_not_hispanic = int(data[i]["Ethnicities"]["White Alone, not Hispanic or Latino"])
-bi_racial = int(data[i]["Ethnicities"]["Two or More Races"])
-asian_alone = int(data[i]["Ethnicities"]["Asian Alone"])
-pacific_islander = int(data[i]["Ethnicities"]["Hispanic or Latino"])
-white_alone = int(data[i]["Ethnicities"]["White Alone"])
-hispanic_alone = int(data[i]["Ethnicities"]["Hispanic or Latino"])
-black_alone = int(data[i]["Ethnicities"]["Black Alone"])
-native_alone = int(data[i]["Ethnicities"]["American Indian and Alaska Native Alone"])
+            white_not_hispanic = int(data[i]["Ethnicities"]["White Alone, not Hispanic or Latino"])
+            bi_racial = int(data[i]["Ethnicities"]["Two or More Races"])
+            asian_alone = int(data[i]["Ethnicities"]["Asian Alone"])
+            pacific_islander = int(data[i]["Ethnicities"]["Hispanic or Latino"])
+            white_alone = int(data[i]["Ethnicities"]["White Alone"])
+            hispanic_alone = int(data[i]["Ethnicities"]["Hispanic or Latino"])
+            black_alone = int(data[i]["Ethnicities"]["Black Alone"])
+            native_alone = int(data[i]["Ethnicities"]["American Indian and Alaska Native Alone"])
 #...diversity
 
 #white_not_hispanic, bi_racial, asian_alone, pacific_islander, white_alone, hispanic_alone, black_alone, native_alone
